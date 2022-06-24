e846d558e16812a4c54447d47e2fcf5f6fbc7810
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-06-24 11:42:49 +0900

    update

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,24 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, './config/config.env') })
+
 const port = 8000
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send()
-})
+app.use(morgan('dev'))
+app.use(express.json({ limit: '30mb' }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
-app.listen(port, () => {
-  console.info(`server running on port ${port}`)
-})
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((conn) => {
+    app.listen(port, () => console.info(`server running ${port}`))
+  })
+  .catch((error) => console.info(`${error} did not connect`))
