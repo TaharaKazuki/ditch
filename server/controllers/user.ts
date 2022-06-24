0e817047c6005b580e78c7585c0c4e5a9ef4a7aa
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-06-24 16:22:43 +0900

    update

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -7,7 +7,25 @@ import type { Request, Response } from 'express'
 dotenv.config({ path: path.resolve(__dirname, '../config/config.env') })
 
 const secret = process.env.SECRET
-console.info('secret', secret)
+
+export const signin = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const oldUser = await UserModel.findOne({ email })
+    if (!oldUser) return res.status(404).json({ message: "User does't exist" })
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password)
+    if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credentials' })
+
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: '1h' })
+
+    res.status(200).json({ result: oldUser, token })
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' })
+    console.error(error)
+  }
+}
 
 export const signup = async (req: Request, res: Response) => {
   const { email, password, firstName, lastName } = req.body
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 const router = Router()
 
-import { signup } from '../controllers/user'
+import { signup, signin } from '../controllers/user'
 
 router.post('/signup', signup)
+router.post('/signin', signin)
 
 export default router
